7e062e5e1e3f3af4d59235af334820a4ed3b1f21
Caio Oliveira <contato@caiodesign.com>
2018-05-22 15:18:24 -0300

    configurado pasta estática do express para /proj

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ var express = require('express')
     ,bodyParser = require('body-parser')
     ,routes = require('../backend/routes');
 
-app.use(express.static('./public'));
+app.use(express.static('./proj'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
